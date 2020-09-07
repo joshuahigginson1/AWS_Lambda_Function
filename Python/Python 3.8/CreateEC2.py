925a1fe90fadb96681450f205bf7f8c598bec76f
Josh Higginson <joshuahigginson1@gmail.com>
2020-09-07 12:02:36 +0100

    Added Function to create an EC2 Instance in Ireland

---
+++ Python/Python 3.8/CreateEC2.py
@@ -0,0 +1,19 @@
+""" An AWS Lambda Function to create an EC2 Instance."""
+
+# Import Boto3.
+import boto3
+
+# Create EC2 Connection
+
+ec2 = boto3.resource('ec2')
+
+# Create a new ec2 instance.
+
+ec2.create_instances(
+    ImageId='ami-04137ed1a354f54c4',
+    MinCount=1,
+    MaxCount=1,
+    InstanceType='t2.micro',
+    KeyName='asbandia-key-pair',
+    AvailabilityZone='eu-west-1a'
+)
---
+++ Python/Python 3.8/HelloWorld.py
@@ -0,0 +1,6 @@
+""" A Hello World Function to test AWS Lambda."""
+
+
+def hello_world():
+    """ This function returns the string 'Hello World!'"""
+    return "Hello World!"
